8758c6710e01ce9074317c1a6fd8b5c57723c0c5
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-17 23:06:45 +0300

    first commit

---
+++ SignIn/SignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInApp.swift
+//  SignIn
+//
+//  Created by Amby on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
